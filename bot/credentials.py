128024b8eaee0545cbdb553419c77914a8a7fd6f
keshav098655 <65311135+keshav098655@users.noreply.github.com>
2021-08-11 10:01:08 +0530

    Update credentials.py

--- bot/credentials.py
+++ bot/credentials.py
@@ -1,16 +1,16 @@
 '''Credentials'''
 
 
-api_id = ''     #API ID
+api_id = '1089626'
 
-api_hash = ''       #API Hash
+api_hash = 'cc277786dbbdc6d2693a3e2abdd47a66'
 
-bot_token = ''      #Bot Token
+bot_token = '1904393503:AAHTdH_Uu4lvXvmDb82erGBRuy-4rL9-CIE'
 
-host = ''       #Host Name of Mysql
+host = 'ec2-54-173-138-144.compute-1.amazonaws.com'
 
-user = ''       #Username of Mysql
+user = 'qgoqkdxnkkwqjm'
 
-password = ''       #Password of Mysql
+password = 'e49db8e00597915e5d728cb3d75a4bca333a64b83768072490d4b8eb462e14dd'
 
-database = ''       #Database of Mysql
+database = 'postgres://qgoqkdxnkkwqjm:e49db8e00597915e5d728cb3d75a4bca333a64b83768072490d4b8eb462e14dd@ec2-54-173-138-144.compute-1.amazonaws.com:5432/d6qv5h9ojf082o'
